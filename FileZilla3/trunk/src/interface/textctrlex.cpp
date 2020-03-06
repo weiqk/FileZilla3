ab63b1fc9099dd2425442c59203dcc0ed9ce9c3d
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2020-03-06 10:10:29 +0000

    Set a default length limit.

--- FileZilla3/trunk/src/interface/textctrlex.cpp
+++ FileZilla3/trunk/src/interface/textctrlex.cpp
@@ -2,13 +2,22 @@
 
 #include "textctrlex.h"
 
-#ifdef __WXMAC__
-
 wxTextCtrlEx::wxTextCtrlEx(wxWindow* parent, int id, wxString const& value, wxPoint const& pos, wxSize const& size, long style)
 	: wxTextCtrl(parent, id, value, pos, size, style)
 {
+	SetMaxLength(512 * 1024);
 }
 
+bool wxTextCtrlEx::Create(wxWindow* parent, int id, wxString const& value, wxPoint const& pos, wxSize const& size, long style)
+{
+	bool ret = wxTextCtrl::Create(parent, id, value, pos, size, style);
+	if (ret) {
+		SetMaxLength(512 * 1024);
+	}
+	return ret;
+}
+
+#ifdef __WXMAC__
 static wxTextAttr DoGetDefaultStyle(wxTextCtrl* ctrl)
 {
 	wxTextAttr style;
@@ -27,5 +36,4 @@ void wxTextCtrlEx::Paste()
 	wxTextCtrl::Paste();
 	SetStyle(0, GetLastPosition(), GetDefaultTextCtrlStyle(this));
 }
-
 #endif
--- FileZilla3/trunk/src/interface/textctrlex.h
+++ FileZilla3/trunk/src/interface/textctrlex.h
@@ -3,23 +3,22 @@
 
 #include <wx/textctrl.h>
 
-#ifdef __WXMAC__
-// Subclassed wxTextCtrl to disable pasting of formatting, we're only ever interested in the text.
 class wxTextCtrlEx : public wxTextCtrl
 {
 public:
 	wxTextCtrlEx() = default;
 	wxTextCtrlEx(wxWindow* parent, int id, wxString const& value = wxString(), wxPoint const& pos = wxDefaultPosition, wxSize const& size = wxDefaultSize, long style = 0);
 
-	virtual void Paste();
+	bool Create(wxWindow* parent, int id, wxString const& value = wxString(), wxPoint const& pos = wxDefaultPosition, wxSize const& size = wxDefaultSize, long style = 0);
+
+#ifdef __WXMAC__
+	// Disable pasting of formatting, we're only ever interested in the text.
+	virtual void Paste() override;
+#endif
 };
 
+#ifdef __WXMAC__
 const wxTextAttr& GetDefaultTextCtrlStyle(wxTextCtrl* ctrl);
-
-#else
-
-#define wxTextCtrlEx wxTextCtrl
-
 #endif
 
 #endif
