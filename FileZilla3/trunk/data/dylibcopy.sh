1c5fe09f687963e41e8f02585c0a71293eda1af2
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2020-09-24 15:06:38 +0000

    Look for shared objects in the engine build directory.

--- FileZilla3/trunk/data/dylibcopy.sh
+++ FileZilla3/trunk/data/dylibcopy.sh
@@ -3,6 +3,7 @@
 set -e
 
 bundle="$1"
+searchdir="$2"
 
 if [ ! -d "$bundle" ]; then
   echo "$bundle is not an application bundle"
@@ -42,7 +43,14 @@ process_dylib()
 
   if [ ! -f "${frameworks}/$name" ] && [ ! -f "${frameworks}/$name.processed" ]; then
     touch "${frameworks}/$name.processed"
-    if [ -f "$dylib" ]; then
+
+    if [ -f "$searchdir/.libs/$name" ]; then
+      echo "Found dependency $name"
+      cp "$searchdir/.libs/$name" "${frameworks}/$name"
+    elif [ -f "$searchdir/$name" ]; then
+      echo "Found dependency $name"
+      cp "$searchdir/$name" "${frameworks}/$name"
+    elif [ -f "$dylib" ]; then
       echo "Found dependency $name"
       cp "$dylib" "${frameworks}/$name"
     else
