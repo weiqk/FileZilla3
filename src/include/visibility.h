4b6cf593f5a1e3f019d1215bcdb74554755ad76a
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2021-01-19 12:32:51 +0000

    Use new visibility helper

--- src/include/visibility.h
+++ src/include/visibility.h
@@ -1,52 +1,19 @@
 #ifndef FILEZILLA_ENGINE_VISIBILITY_HEADER
 #define FILEZILLA_ENGINE_VISIBILITY_HEADER
 
-// For FZ_WINDOWS/MAC/UNIX defines
-#include <libfilezilla/libfilezilla.hpp>
+#include <libfilezilla/visibility_helper.hpp>
 
 // Symbol visibility. There are two main cases: Building FileZilla and using it
 #ifdef BUILDING_FILEZILLA
-
-  // Two cases when building: Windows, other platform
-  #ifdef FZ_WINDOWS
-
-    // Under Windows we can either use Visual Studio or a proper compiler
-    #ifdef _MSC_VER
-      #ifdef DLL_EXPORT
-        #define FZC_PUBLIC_SYMBOL __declspec(dllexport)
-      #endif
-    #else
-      #ifdef DLL_EXPORT
-        #define FZC_PUBLIC_SYMBOL __declspec(dllexport)
-      #else
-        #define FZC_PUBLIC_SYMBOL __attribute__((visibility("default")))
-        #define FZC_PRIVATE_SYMBOL __attribute__((visibility("hidden")))
-      #endif
-    #endif
-
-  #else
-
-    #define FZC_PUBLIC_SYMBOL __attribute__((visibility("default")))
-    #define FZC_PRIVATE_SYMBOL __attribute__((visibility("hidden")))
-
-  #endif
-
+  #define FZC_PUBLIC_SYMBOL FZ_EXPORT_PUBLIC
+  #define FZC_PRIVATE_SYMBOL FZ_EXPORT_PRIVATE
 #else
-
-  // Under MSW it makes a difference whether we use a static library or a DLL
-  #if defined(FZ_WINDOWS) && defined(FZC_USING_DLL)
-    #define FZC_PUBLIC_SYMBOL __declspec(dllimport)
+  #if FZC_USING_DLL
+    #define FZC_PUBLIC_SYMBOL FZ_IMPORT_SHARED
+  #else
+    #define FZC_PUBLIC_SYMBOL FZ_IMPORT_STATIC
   #endif
-
-#endif
-
-
-#ifndef FZC_PUBLIC_SYMBOL
-#define FZC_PUBLIC_SYMBOL
-#endif
-
-#ifndef FZC_PRIVATE_SYMBOL
-#define FZC_PRIVATE_SYMBOL
+  #define FZC_PRIVATE_SYMBOL
 #endif
 
 #endif
